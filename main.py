e8c48a89cae5791d8388d4b3d4c04ac4501ceab5
Ahmad Tameem <ahmad.tameem97@gmail.com>
2023-03-19 01:35:59 +0500

    Multiple Execution Mode Addition

--- main.py
+++ main.py
@@ -1,10 +1,54 @@
 from my_parser import my_parser
 from json import dumps
+import sys
+import os
 
-NewParser = my_parser()
 
-program = '''  
- '''
+# Recursive Decent Parser
+rParser = my_parser()
 
-parse_ast = NewParser.parse(program)
-print(dumps(parse_ast, indent=2))
+
+mode = None
+print_ast = False
+raw_string = ""
+
+if "--t" in sys.argv: mode = "-test"
+if "--p" in sys.argv: print_ast = True
+print(sys.argv)
+if ("--t" not in sys.argv and len(sys.argv)==2): mode = "-file"
+
+
+if (mode=='-test'):
+    # Run Tests in the directory
+    dir = os.listdir("./Tests")
+
+    for i in dir:
+        read_file = open("./Tests/"+i, "r")
+
+        print("\nRunning Test: ", "Tests/"+i)   
+        parse_ast = rParser.parse(read_file.read())
+
+        if (print_ast): print(dumps(parse_ast, indent=2))
+
+        #print(a.parse(b))
+        read_file.close()
+        
+    print("\nAll Tests Passed...\nTotal Tests:", len(dir))
+
+elif (mode == "-file"):
+    # Execute the file
+    read_file = open(sys.argv[1], "r")
+    parse_ast = rParser.parse(read_file.read())
+    print(dumps(parse_ast, indent=2))
+    read_file.close()
+
+else:
+    # User-Input
+    raw_string = input(">>> ")
+    st = "\n"
+    while (st != ""): 
+        st = input(">>> ")
+        raw_string += "\n" + st
+    parse_ast = rParser.parse(raw_string)
+        
+    print(dumps(parse_ast, indent=2))
