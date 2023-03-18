44aa1cfe22ca5c2cb651326e6f1945a09a4e161a
Ahmad Tameem <ahmad.tameem97@gmail.com>
2023-03-18 21:45:56 +0500

    Added Numeric Literal

--- my_parser.py
+++ my_parser.py
@@ -1,9 +1,24 @@
 import tokenizer
 
+
+# recursive decent parser
 class my_parser:
     def __init__(self) -> None:
-        pass
+        self._string = ""
 
 
+    # parse a string into AST
     def parse(self, string):
-        pass
+        self._string = string
+        return self.program()
+
+
+    # main entry point
+    def program(self):
+        return self.numeric_literal()
+    
+
+    # Numeric Literal
+    def numeric_literal(self):
+        return {'type': 'NumericLiteral',
+                 'value': int(self._string)}
